354c5f8fd427f1f17f28f15ca8fb225129364e5c
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-06-19 11:50:54 +0530

    Update storefile.py
    Fixed Protect Content 🙃

--- plugins/storefile.py
+++ plugins/storefile.py
@@ -56,7 +56,7 @@ async def storefile(c, m):
     # sending message
     await send_message.edit(
         text,
-        reply_markup=InlineKeyboardMarkup(buttons)
+        reply_markup=InlineKeyboardMarkup(buttons),
         protect_content=PROTECT_CONTENT
     )
 
